89e1a89300d6a6c64d21114f2ba196d0ea67f1f2
Donghwoo Cho <67030031+DonghwooCho@users.noreply.github.com>
2022-12-30 03:24:40 +0900

    [Bronze II] Title: 분해합, Time: 1512 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -6,11 +6,14 @@ N = int(sys.stdin.readline().rstrip())
 # 2. 생성자의 최솟값 출력
 target = []
 min = 0
-for i in range(N, 1, -1):
+for i in range(1, N):
     target = list(map(int, list(str(i))))
-    for j in range(0, len(target)):
-        target[j] = int(target[j]) * ((10 ** (len(target) - j - 1)) + 1)
-    if N == sum(target):
+    # for j in range(0, len(target)):
+    #     target[j] = int(target[j]) * ((10 ** (len(target) - j - 1)) + 1)
+    # if N == sum(target):
+    #     min = i
+    if N == sum(target) + i:
         min = i
-    
+        break
+
 print(min)
