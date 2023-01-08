31f404bc33871d296662b3a69439a14abc7da7be
Donghwoo Cho <67030031+DonghwooCho@users.noreply.github.com>
2023-01-09 01:57:35 +0900

    [Gold V] Title: AC, Time: 344 ms, Memory: 40372 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+# 1. 입력 받기
+T = int(input())
+
+# 2. AC 수행 결과 출력
+for i in range(T):
+    p = input()
+    n = int(input())
+    arr = input()
+    
+    if len(arr) >= 2:
+        arr = deque(list(arr[1:-1].split(',')))
+    else:
+        print('error')
+        continue
+
+    if p.count('D') > n:
+        print('error')
+    elif p.count('D') == n:
+        print('[]')
+        continue
+    else:
+        arrow = True
+        for j in range(len(p)):
+            if p[j] == 'R':
+                arrow = not arrow
+            else:
+                if arrow:
+                    arr.popleft()
+                else:
+                    arr.pop()
+        if p.count('R') % 2 == 1:
+            arr = deque(list(reversed(arr)))
+            
+        print('[', end = '')
+        for j in range(len(arr) - 1):
+            print(arr[0], end = ',')
+            arr.popleft()
+        print(arr[0], end = '')
+        print(']')
