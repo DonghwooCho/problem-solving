13cf96390782b1f9c8918f659be6cc1e6269f8a0
Donghwoo Cho <67030031+DonghwooCho@users.noreply.github.com>
2023-01-11 21:42:33 +0900

    [Silver IV] Title: 수 찾기, Time: 188 ms, Memory: 51468 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -4,15 +4,14 @@ from collections import Counter
 # 1. 입력 받기
 N = int(sys.stdin.readline().rstrip())
 
-A = list(map(int, (sys.stdin.readline().rstrip().split())))
-A_count = Counter(A)
+A = set(map(int, (sys.stdin.readline().rstrip().split())))
 
 # 2. 존재 여부 출력 하기
 M = int(sys.stdin.readline().rstrip())
 
 targets = list(map(int, (sys.stdin.readline().rstrip().split())))
 for target in targets:
-    if A_count[target] != 0:
+    if target in A:
         print(1)
     else:
         print(0)
