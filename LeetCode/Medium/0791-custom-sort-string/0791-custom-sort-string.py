d06e40da724cb2ed0e692f38b1c7eb874d55fbed
Donghwoo Cho <67030031+DonghwooCho@users.noreply.github.com>
2023-01-22 20:33:38 +0900

    Time: 35 ms (61.18%), Space: 13.9 MB (62.76%) - LeetHub

--- LeetCode/Medium/0791-custom-sort-string/0791-custom-sort-string.py
+++ LeetCode/Medium/0791-custom-sort-string/0791-custom-sort-string.py
@@ -2,6 +2,5 @@ class Solution:
     def customSortString(self, order: str, s: str) -> str:
         order = list(order)
         for i in range(len(order) - 1, -1, -1):
-            cnt = s.count(order[i])
-            s = order[i] * cnt + s.replace(order[i], '')
+            s = order[i] * s.count(order[i]) + s.replace(order[i], '')
         return s
