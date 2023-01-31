c667140131539050c1c4d3ddafa84eb49c41e50e
Donghwoo Cho <67030031+DonghwooCho@users.noreply.github.com>
2023-01-31 17:07:50 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -2,22 +2,12 @@
 target = input()
 
 # 2. Output result
-count = target.count("c=")          \
-      + target.count("c-")          \
-      + target.count("z=")          \
-      + target.count("d-")          \
-      + target.count("lj")          \
-      + target.count("nj")          \
-      + target.count("s=")
-
-length = (len(target) -
-       (target.count("c=") * 2
-       + target.count("c-") * 2
-       + target.count("z=") * 2
-       + target.count("dz=") * 1
-       + target.count("d-") * 2
-       + target.count("lj") * 2
-       + target.count("nj") * 2
-       + target.count("s=") * 2))
-
-print(count + length)
+print((len(target) -
+       (target.count("c=")
+       + target.count("c-")
+       + target.count("z=")
+       + target.count("dz=")
+       + target.count("d-")
+       + target.count("lj")
+       + target.count("nj")
+       + target.count("s="))))
